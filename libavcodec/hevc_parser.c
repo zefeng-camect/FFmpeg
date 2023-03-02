zefeng-camect/FFmpeg

commit 29107fa2168da0fcb4f04252104f04a3ef010394

Make it work when VPS is not specified. Many new cameras (like uniview) generate streams like this.

libavcodec/hevcdec.c
@@ -318,7 +318,7 @@ static void export_stream_params(HEVCContext *s, const HEVCSPS *sps)
 {
     AVCodecContext *avctx = s->avctx;
     const HEVCParamSets *ps = &s->ps;
-    const HEVCVPS *vps = (const HEVCVPS*)ps->vps_list[sps->vps_id]->data;
+    HEVCVPS *vps = NULL;
     const HEVCWindow *ow = &sps->output_window;
     unsigned int num = 0, den = 0;
 
@@ -358,7 +358,10 @@ static void export_stream_params(HEVCContext *s, const HEVCSPS *sps)
             avctx->chroma_sample_location = AVCHROMA_LOC_LEFT;
     }
 
-    if (vps->vps_timing_info_present_flag) {
+    if (ps->vps_list && ps->vps_list[sps->vps_id]) {
+        vps = (const HEVCVPS*)ps->vps_list[sps->vps_id]->data;
+    }
+    if (vps && vps->vps_timing_info_present_flag) {
         num = vps->vps_num_units_in_tick;
         den = vps->vps_time_scale;
     } else if (sps->vui.vui_timing_info_present_flag) {

libavcodec/hevc_ps.c
@@ -919,9 +919,7 @@ int ff_hevc_parse_sps(HEVCSPS *sps, GetBitContext *gb, unsigned int *sps_id,
     sps->vps_id = get_bits(gb, 4);
 
     if (vps_list && !vps_list[sps->vps_id]) {
-        av_log(avctx, AV_LOG_ERROR, "VPS %d does not exist\n",
-               sps->vps_id);
-        return AVERROR_INVALIDDATA;
+        av_log(avctx, AV_LOG_WARNING, "VPS %d does not exist\n", sps->vps_id);
     }
 
     sps->max_sub_layers = get_bits(gb, 3) + 1;

libavformat/hevc.c
@@ -897,7 +897,7 @@ static int hvcc_write(AVIOContext *pb, HEVCDecoderConfigurationRecord *hvcc)
         default:
             break;
         }
-    if (!vps_count || vps_count > HEVC_MAX_VPS_COUNT ||
+    if (vps_count > HEVC_MAX_VPS_COUNT ||
         !sps_count || sps_count > HEVC_MAX_SPS_COUNT ||
         !pps_count || pps_count > HEVC_MAX_PPS_COUNT)
         return AVERROR_INVALIDDATA;
@@ -1085,8 +1085,10 @@ int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data,
     }
 
     ret = ff_avc_parse_nal_units_buf(data, &start, &size);
-    if (ret < 0)
+    if (ret < 0) {
+        av_log(NULL, AV_LOG_ERROR, "ff_avc_parse_nal_units_buf failed with %d\n", ret);
         return ret;
+    }
 
     hvcc_init(&hvcc);
 
@@ -1106,8 +1108,10 @@ int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data,
         case HEVC_NAL_SEI_PREFIX:
         case HEVC_NAL_SEI_SUFFIX:
             ret = hvcc_add_nal_unit(buf, len, ps_array_completeness, &hvcc);
-            if (ret < 0)
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "hvcc_add_nal_unit failed with %d\n", ret);
                 goto end;
+            }
             break;
         default:
             break;

commit f3a4b11f9ad9a875dd71ad2e86e00828b9eca984

Fix crash bugs caused by 29107fa2168da0fcb4f04252104f04a3ef010394

libavcodec/hevc_parser.c
@@ -86,7 +86,9 @@ static int hevc_parse_slice_header(AVCodecParserContext *s, H2645NAL *nal,
     }
     if (ps->sps != (HEVCSPS*)ps->sps_list[ps->pps->sps_id]->data) {
         ps->sps = (HEVCSPS*)ps->sps_list[ps->pps->sps_id]->data;
-        ps->vps = (HEVCVPS*)ps->vps_list[ps->sps->vps_id]->data;
+        if (ps->vps_list && ps->vps_list[ps->sps->vps_id]) {
+            ps->vps = (HEVCVPS*)ps->vps_list[ps->sps->vps_id]->data;
+        }
     }
     ow  = &ps->sps->output_window;
 
@@ -98,7 +100,7 @@ static int hevc_parse_slice_header(AVCodecParserContext *s, H2645NAL *nal,
     avctx->profile  = ps->sps->ptl.general_ptl.profile_idc;
     avctx->level    = ps->sps->ptl.general_ptl.level_idc;
 
-    if (ps->vps->vps_timing_info_present_flag) {
+    if (ps->vps && ps->vps->vps_timing_info_present_flag) {
         num = ps->vps->vps_num_units_in_tick;
         den = ps->vps->vps_time_scale;
     } else if (ps->sps->vui.vui_timing_info_present_flag) {

libavcodec/hevcdec.c
@@ -525,7 +525,9 @@ static int set_sps(HEVCContext *s, const HEVCSPS *sps,
     }
 
     s->ps.sps = sps;
-    s->ps.vps = (HEVCVPS*) s->ps.vps_list[s->ps.sps->vps_id]->data;
+    if (s->ps.vps_list && s->ps.vps_list[s->ps.sps->vps_id]) {
+        s->ps.vps = (HEVCVPS*) s->ps.vps_list[s->ps.sps->vps_id]->data;
+    }
 
     return 0;
 